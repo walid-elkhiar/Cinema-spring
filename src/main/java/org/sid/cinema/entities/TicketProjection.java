162eeaf6d5287c1582e61d10a7c47ba106687cfb
elkhiar.walid@gmail.com <elkhiar.walid@gmail.com>
2021-07-03 00:57:59 +0200

    mon premier commit

---
+++ src/main/java/org/sid/cinema/entities/TicketProjection.java
@@ -0,0 +1,17 @@
+package org.sid.cinema.entities;
+
+
+import org.springframework.data.rest.core.config.Projection;
+
+
+
+@Projection(name = "ticketsProj",types = Ticket.class)
+public interface TicketProjection {
+	public Long getId();
+	public String getNomClient();
+	public double getPrix();
+	public Integer getCodePayement();
+	public boolean getReservee();
+	public Place getPlace();
+	
+}
